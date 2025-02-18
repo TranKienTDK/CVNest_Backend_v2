220c066ba90fc3bc216af10c705c333182905e8d
harryberlin <kien8b2003@gmail.com>
2025-02-18 17:32:29 +0700

    Create source base

---
+++ src/test/java/com/harryberlin/cvnest/CvNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harryberlin.cvnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
